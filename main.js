257d5146e6beb27e77f934d1bf6cd3dc31ad8224
David Hirtle <david.hirtle@yardi.com>
2017-01-16 22:38:06 -0800

    extract css and js to separate files

---
+++ main.js
@@ -0,0 +1,98 @@
+let data;
+let headers = [];
+
+const searchButtonDom = document.querySelector(".voice-search-input");
+const searchOptionsDom = document.querySelector(".voice-search-options");
+const searchResultsDom = document.querySelector(".voice-search-results tbody");
+const summaryDom = document.querySelector(".voice-search-summary");
+
+const searchOptionTemplate = Handlebars.compile(document.getElementById("search-option-template").innerHTML);
+const searchResultTemplate = Handlebars.compile(document.getElementById("search-result-template").innerHTML);
+
+loadData("data/voice-actors.csv");
+
+function loadData(url) {
+	Papa.parse(url, {
+		download: true,
+		header: true,
+		dynamicTyping: true,
+		skipEmptyLines: true,
+		complete: function (results) {
+			data = results.data;
+			headers = results.meta.fields.filter(function (field) {
+				return field && field !== "Name" && field !== "Sample";
+			});
+
+			displaySearchOptions(headers);
+			initInputs();
+			search();
+		}
+	});
+}
+
+function initInputs() {
+	searchButtonDom.addEventListener("input", search);
+	searchOptionsDom.addEventListener("change", search);
+}
+
+function getCheckedOptions() {
+	const checked = document.querySelectorAll('input[type="checkbox"]:checked');
+	return Array.prototype.map.call(checked, function (e) { 
+		return e.value;
+	});
+}
+
+function search(input) {
+	const searchInput = searchButtonDom.value;
+	let matched = data.filter(function (row) {
+		return row.Name.toLowerCase().includes(searchInput.toLowerCase());
+	});
+	
+	const checkedOptions = getCheckedOptions();
+	if (checkedOptions.length) {
+		matched = matched.filter(function (row) {
+			return checkedOptions.every(function (checkedOption) {
+				return row[checkedOption] === true;
+			});
+		});			
+	}
+
+	displaySummary("Matched " + matched.length + " " + maybePlural("actor", matched.length) + ":");
+	displaySearchResults(matched);
+}
+
+function maybePlural(singular, count) {
+	return count === 1 ? singular : singular + "s";
+}
+
+function displaySearchOptions(keys) {
+	const form = document.querySelector(".voice-search-options");
+	form.innerHTML = keys.map(function (key) {
+		return searchOptionTemplate({ key: key, label: prettifyOptionKey(key) });
+	}).join("\n");
+}
+
+function displaySummary(text) {
+	summaryDom.innerText = text;
+}
+
+function displaySearchResults(rows) {
+	const templateModel = rows.map(function (row) {
+		return {
+			name: row.Name,
+			sample: row.Sample,
+			headers: headers.filter(function (header) {
+				return row[header];
+			}).sort().map(prettifyOptionKey).join(", ")
+		};
+		
+	});
+	
+	searchResultsDom.innerHTML = templateModel.map(function (object) {
+		return searchResultTemplate(object);
+	}).join("\n");
+}
+
+function prettifyOptionKey(key) {
+	return key.replace("_", " ");
+}
