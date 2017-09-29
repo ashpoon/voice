91bdbd6dfeba67af072a9704147970ef68ebc33d
David Hirtle <wachunga@gmail.com>
2017-09-29 10:02:11 -0700

    include papaparse in the bundle
    good to be comprehensive with our dependency list
    I could use a higher-order component to abstract the data loading away,
    but not that useful when only used in one place

--- js/app.js
+++ js/app.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Papa from 'papaparse';
 
 import SearchBox from './searchBox';
 import SearchResults from './searchResults';
 
 const searchOptionPrefix = "$";
+const dataUrl = 'data/voice-actors.csv';
 
 class VoiceApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = { data: [], searched: ''};
-    this.loadData('data/voice-actors.csv');
-
     this.onSearchInput = this.onSearchInput.bind(this);
   }
 
-  loadData(url) {
-    Papa.parse(url, {
+  componentDidMount() {
+    this.loadData(dataUrl, results => {
+      const sorted = results.data.sort((a, b) => a.Name.localeCompare(b.Name));
+      console.log(results)
+      this.setState({ data: sorted })
+    })
+  }
+
+  loadData(url, done) {
+    return Papa.parse(url, {
       download: true,
       header: true,
       dynamicTyping: true,
       skipEmptyLines: true,
-      complete: results => {
-        const sorted = results.data.sort(function (a, b) {
-          return a.Name.localeCompare(b.Name);
-        });
-        console.log('done loading')
-        console.log(results)
-        this.setState({ data: sorted })
+      complete: done
+    })
+
         // headers = results.meta.fields.filter(function identifySearchOption(field) {
         //   return field[0] === searchOptionPrefix;
         // }).map(function stripSearchOptionPrefix(field) {
@@ -35,10 +39,6 @@ class VoiceApp extends React.Component {
         // });
 
         // displaySearchOptions(headers);
-        // initInputs();
-        // search();
-      }
-    });
   }
 
   onSearchInput(searched) {
