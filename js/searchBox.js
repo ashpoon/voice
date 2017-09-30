c58baa36e8c640119d062a522498320c9f1b43c5
David Hirtle <wachunga@gmail.com>
2017-09-30 15:57:19 -0700

    IE was missing eg Array.includes
    better approach would be to conditionally load a separate polyfill

--- js/searchBox.js
+++ js/searchBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 function SearchBox (props) {
   return (
     <input type='text' className='voice-search-input form-control'
-      placeholder='Search...' autoComplete='off' autofocus='true'
+      placeholder='Search by name...' autoComplete='off' autofocus='true'
       value={props.value} onChange={props.onChange} />
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  entry: [
-    './js/app.js'
-  ],
+  entry: {
+    bundle: [
+      './js/app.js',
+      // below are just polyfills for eg IE
+      'core-js/es6/array',  // eg Array.from
+      'core-js/fn/object/assign',
+      'core-js/fn/array/includes'
+    ]
+  },
   output: {
     // path: path.join(__dirname, 'www'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
